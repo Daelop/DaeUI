5ad76329bc058d06dd591f959394f499e4cf0d6e
Daelop <jackdoddpost@gmail.com>
2024-04-20 06:53:43 +1200

    Updated fetch hook with default behavior.

--- Components/Hooks/Fetch.tsx
+++ Components/Hooks/Fetch.tsx
@@ -11,10 +11,23 @@ export default function Fetch({type, url, data, callback, error}) {
         })
         .then((response) => response.json())
         .then((data) => {
-            callback(data);
+            //allows for a callback function to be passed in, if not, it will log the data to the console and return it.
+            if (callback){
+             callback(data);
+            }else{
+                console.log(data);
+                return data;
+            }
+            
         })
         .catch((err) => {
-            error(err);
+            //allows for an error function to be passed in, if not, it will log the error to the console and return it.
+            if (error) {
+                error(err);
+            }else{
+                console.log(err);
+                return err;
+            }
         });
     }, []);
 }
